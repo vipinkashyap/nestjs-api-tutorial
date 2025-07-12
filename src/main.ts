3e38b54fe82de623b841f20ace17e75e0b1795a2
Vipin Kashyap <kaizen.labsindia@gmail.com>
2025-07-11 21:11:17 -0700

    This is global validation for objects exchanged in network requests.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Automatically remove properties that do not have any decorators
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
